12882ed4c2250fc17c4469f25d9dd2cb20dbf338
SomeMinecraftModder <staff@webgarbage.fr>
2022-09-01 15:09:25 +0200

    Added click and some refactor

--- aproxFinder/main.py
+++ aproxFinder/main.py
@@ -1,5 +1,5 @@
-import math
 import operator
+import click
 from tqdm import tqdm
 
 
@@ -13,18 +13,24 @@ def rate_approximation(x, approximation):
     return abs(approximation - x)
 
 
-def generate_approximation():
+@click.command()
+@click.option('--num', help='Number to approximate', required=True)
+def generate(num):
+    """Generate an approximation"""
+    if not num.isnumeric():
+        click.echo("--num must be a number")
+        return
+    num = int(num)
     score_dict = {}
     for x in tqdm(range(1, 1000)):
         for y in range(1, 1000):
-            score = rate_approximation(x/y, math.e)
+            score = rate_approximation(x/y, num)
             score_dict[score] = [x, y]
     best = min(score_dict.items(), key=operator.itemgetter(0))
     common = gcd(best[1][0], best[1][1])
     best[1][0] //= common
     best[1][1] //= common
-    print(best)
-
-
-if __name__ == "__main__":
-    generate_approximation()
+    click.echo("     Result     ")
+    click.echo("-"*17)
+    click.echo(f"Score (lowest is better): {best[0]}")
+    click.echo(f"Approximation (fraction): {best[1]}")
